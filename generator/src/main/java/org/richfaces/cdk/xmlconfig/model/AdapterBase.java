4b40f0038ad07b0b07fa2e4793a762cdbb9b79b1
Luca Stancapiano <l.stancapiano@sourcesense.com>
2018-06-22 21:05:10 +0200

    upgrade to java 10 and jsf 2.3

--- generator/src/main/java/org/richfaces/cdk/xmlconfig/model/AdapterBase.java
+++ generator/src/main/java/org/richfaces/cdk/xmlconfig/model/AdapterBase.java
@@ -22,6 +22,7 @@
  */
 package org.richfaces.cdk.xmlconfig.model;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
@@ -38,149 +39,153 @@ import org.richfaces.cdk.util.JavaUtils;
  * @author asmirnov@exadel.com
  */
 public abstract class AdapterBase<Bean, Model> extends XmlAdapter<Bean, Model> {
-    @Override
-    public Bean marshal(Model model) throws CdkException {
-        Bean bean = null;
-        try {
-            bean = createBean(getBeanClass(model), model);
-        } catch (ClassCastException e) {
-            e.printStackTrace();
-        }
-        postMarshal(model, bean);
-        return bean;
-    }
+	@Override
+	public Bean marshal(Model model) throws CdkException {
+		Bean bean = null;
+		try {
+			bean = createBean(getBeanClass(model), model);
+		} catch (ClassCastException e) {
+			e.printStackTrace();
+		}
+		postMarshal(model, bean);
+		return bean;
+	}
 
-    /**
-     * <p class="changed_added_4_0">
-     * This method creates adapter object and copies properties from model object to adapter.
-     * </p>
-     *
-     * @param beanClass adapter class.
-     * @param model model object class.
-     * @return initialized instance of adapter object.
-     *
-     * @throws org.richfaces.cdk.CdkException
-     */
-    @SuppressWarnings("unchecked")
-    public Bean createBean(Class<? extends Bean> beanClass, Model model) throws CdkException {
-        try {
-            Bean bean = beanClass.newInstance();
+	/**
+	 * <p class="changed_added_4_0">
+	 * This method creates adapter object and copies properties from model object to
+	 * adapter.
+	 * </p>
+	 *
+	 * @param beanClass
+	 *            adapter class.
+	 * @param model
+	 *            model object class.
+	 * @return initialized instance of adapter object.
+	 *
+	 * @throws org.richfaces.cdk.CdkException
+	 */
+	@SuppressWarnings("unchecked")
+	public Bean createBean(Class<? extends Bean> beanClass, Model model) throws CdkException {
+		try {
+			Bean bean = beanClass.getConstructor().newInstance();
 
-            // Copy properties from model to bean.
-            JavaUtils.copyProperties(model, bean);
-            if (model instanceof Extensible && bean instanceof Extensible) {
-                copyExtensions((Extensible) model, (Extensible) bean, true);
-            }
+			// Copy properties from model to bean.
+			JavaUtils.copyProperties(model, bean);
+			if (model instanceof Extensible && bean instanceof Extensible) {
+				copyExtensions((Extensible) model, (Extensible) bean, true);
+			}
 
-            return bean;
-        } catch (InstantiationException e) {
-            throw new CdkException("JAXB adapter class instantiation error", e);
-        } catch (IllegalAccessException e) {
-            throw new CdkException("JAXB adapter class instantiation error", e);
-        }
-    }
+			return bean;
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			throw new CdkException("JAXB adapter class instantiation error", e);
+		}
+	}
 
-    /**
-     * <p class="changed_added_4_0">
-     * Template method to copy non bean attributes.
-     * </p>
-     *
-     * @param model
-     * @param bean
-     */
-    protected void postMarshal(Model model, Bean bean) {
-        // template method to perform additional conversations
-    }
+	/**
+	 * <p class="changed_added_4_0">
+	 * Template method to copy non bean attributes.
+	 * </p>
+	 *
+	 * @param model
+	 * @param bean
+	 */
+	protected void postMarshal(Model model, Bean bean) {
+		// template method to perform additional conversations
+	}
 
-    /**
-     * <p class="changed_added_4_0">
-     * Returns concrete bean class.
-     * </p>
-     *
-     * @param model
-     * @return
-     */
-    protected abstract Class<? extends Bean> getBeanClass(Model model);
+	/**
+	 * <p class="changed_added_4_0">
+	 * Returns concrete bean class.
+	 * </p>
+	 *
+	 * @param model
+	 * @return
+	 */
+	protected abstract Class<? extends Bean> getBeanClass(Model model);
 
-    @Override
-    public Model unmarshal(Bean bean) throws CdkException {
-        Model model = createModelElement(getModelClass(bean), bean);
-        postUnmarshal(bean, model);
-        return model;
-    }
+	@Override
+	public Model unmarshal(Bean bean) throws CdkException {
+		Model model = createModelElement(getModelClass(bean), bean);
+		postUnmarshal(bean, model);
+		return model;
+	}
 
-    @SuppressWarnings("unchecked")
-    protected <D, E extends ConfigExtension> E createExtension(D destination) throws NoSuchMethodException,
-            InstantiationException, IllegalAccessException {
+	@SuppressWarnings("unchecked")
+	protected <D, E extends ConfigExtension> E createExtension(D destination)
+			throws NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, SecurityException {
 
-        Method method = destination.getClass().getMethod("getExtension");
-        return ((Class<E>) method.getReturnType()).newInstance();
-    }
+		Method method = destination.getClass().getMethod("getExtension");
+		return ((Class<E>) method.getReturnType()).getConstructor().newInstance();
+	}
 
-    private void copyExtensions(Extensible<ConfigExtension> source, Extensible<ConfigExtension> destination, Boolean fromModel) {
-        try {
-            ConfigExtension sourceExtension = source.getExtension();
+	private void copyExtensions(Extensible<ConfigExtension> source, Extensible<ConfigExtension> destination,
+			Boolean fromModel) {
+		try {
+			ConfigExtension sourceExtension = source.getExtension();
 
-            if (null != sourceExtension) {
-                ConfigExtension destinationExtension = createExtension(destination);
+			if (null != sourceExtension) {
+				ConfigExtension destinationExtension = createExtension(destination);
 
-                destinationExtension.setExtensions(sourceExtension.getExtensions());
+				destinationExtension.setExtensions(sourceExtension.getExtensions());
 
-                if (fromModel) {
-                    JavaUtils.copyProperties(source, destinationExtension);
-                } else {
-                    JavaUtils.copyProperties(sourceExtension, destination);
-                }
+				if (fromModel) {
+					JavaUtils.copyProperties(source, destinationExtension);
+				} else {
+					JavaUtils.copyProperties(sourceExtension, destination);
+				}
 
-                destination.setExtension(destinationExtension);
-            } else if (fromModel) {
-                ConfigExtension destinationExtension = createExtension(destination);
+				destination.setExtension(destinationExtension);
+			} else if (fromModel) {
+				ConfigExtension destinationExtension = createExtension(destination);
 
-                JavaUtils.copyProperties(source, destinationExtension);
-                destination.setExtension(destinationExtension);
-            }
-        } catch (Exception e) {
-            throw new CdkException("Properties copiing error", e);
-        }
-    }
+				JavaUtils.copyProperties(source, destinationExtension);
+				destination.setExtension(destinationExtension);
+			}
+		} catch (Exception e) {
+			throw new CdkException("Properties copiing error", e);
+		}
+	}
 
-    @SuppressWarnings("unchecked")
-    protected Model createModelElement(Class<? extends Model> modelClass, Bean adapter) {
-        try {
+	@SuppressWarnings("unchecked")
+	protected Model createModelElement(Class<? extends Model> modelClass, Bean adapter) {
+		try {
 
-            Model modelBean = modelClass.newInstance();
+			Model modelBean = modelClass.getConstructor().newInstance();
 
-            JavaUtils.copyProperties(adapter, modelBean);
+			JavaUtils.copyProperties(adapter, modelBean);
 
-            if (adapter instanceof Extensible && modelBean instanceof Extensible) {
-                copyExtensions((Extensible) adapter, (Extensible) modelBean, false);
-            }
+			if (adapter instanceof Extensible && modelBean instanceof Extensible) {
+				copyExtensions((Extensible) adapter, (Extensible) modelBean, false);
+			}
 
-            return modelBean;
-        } catch (Exception e) {
-            throw new CdkException("CDK model class instantiation error", e);
-        }
-    }
+			return modelBean;
+		} catch (Exception e) {
+			throw new CdkException("CDK model class instantiation error", e);
+		}
+	}
 
-    /**
-     * <p class="changed_added_4_0">
-     * Template method to copy non bean attributes
-     * </p>
-     *
-     * @param bean
-     * @param model
-     */
-    protected void postUnmarshal(Bean bean, Model model) {
-        // template method to perform additional conversations
-    }
+	/**
+	 * <p class="changed_added_4_0">
+	 * Template method to copy non bean attributes
+	 * </p>
+	 *
+	 * @param bean
+	 * @param model
+	 */
+	protected void postUnmarshal(Bean bean, Model model) {
+		// template method to perform additional conversations
+	}
 
-    /**
-     * <p class="changed_added_4_0">
-     * Returns concrete model class
-     * </p>
-     *
-     * @param bean
-     * @return
-     */
-    protected abstract Class<? extends Model> getModelClass(Bean bean);
+	/**
+	 * <p class="changed_added_4_0">
+	 * Returns concrete model class
+	 * </p>
+	 *
+	 * @param bean
+	 * @return
+	 */
+	protected abstract Class<? extends Model> getModelClass(Bean bean);
 }
